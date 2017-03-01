6d734d8075eff4a20c632389ba939edac4785fb0
Tiago Rosado <tiagocrosado@tecnico.ulisboa.pt>
2017-03-01 14:26:19 +0000

    Added Maven Server Hello world test

---
+++ server/src/main/java/pt/ulisboa/tecnico/sec/tg11/Main.java
@@ -0,0 +1,13 @@
+package pt.ulisboa.tecnico.sec.tg11;
+
+/**
+ * Created by trosado on 01/03/17.
+ *
+ */
+public class Main {
+
+    public static void main(String [] args){
+        System.out.println("This is Server");
+
+    }
+}
