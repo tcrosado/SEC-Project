70be1cc1ebb071393153dd620369ad984752015d
Tiago Rosado <tiagocrosado@tecnico.ulisboa.pt>
2017-03-03 20:51:54 +0000

    Added basic library implementation

--- pwmlib/src/main/java/pt/ulisboa/tecnico/sec/tg11/PwmLib.java
+++ pwmlib/src/main/java/pt/ulisboa/tecnico/sec/tg11/PwmLib.java
@@ -1,65 +1,31 @@
 package pt.ulisboa.tecnico.sec.tg11;
 
+import javax.print.DocFlavor;
 import java.rmi.RemoteException;
 import java.rmi.registry.LocateRegistry;
 import java.rmi.registry.Registry;
 import java.security.*;
+import java.util.UUID;
 
 /**
  * Created by trosado on 01/03/17.
  */
 public class PwmLib {
-    private KeyStore ks;
-
+    private final String CLIENT_PUBLIC_KEY = "CLIENT_PUBLIC_KEY";
+    private char[] ksPassword;
+    private KeyStore ks = null;
+    private UUID userID = null;
+    private ServerInterface server = null;
 
 
     public static void main(String[] args){
 
 
 
-        String text = "RMI Test Message";
-        ServerInterface server = null;
-
-        try {
-
-
-
-            Registry registry = LocateRegistry.getRegistry("127.0.0.1", 1099);
-            server = (ServerInterface) registry.lookup("PWMServer");
-            System.out.println("Connected to Server");
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        if (server != null) {
-            try {
-                KeyPairGenerator keyGen = KeyPairGenerator.getInstance("DSA", "SUN");
-                SecureRandom random = SecureRandom.getInstance("SHA1PRNG", "SUN");
-                keyGen.initialize(1024, random);
-
-                KeyPair keypair = keyGen.genKeyPair();
-
-                server.put(keypair.getPublic(),new byte[0],new byte[0],new byte[0]);
-
-            } catch (RemoteException e) {
-                e.printStackTrace();
-            } catch (NoSuchAlgorithmException e) {
-                e.printStackTrace();
-            } catch (NoSuchProviderException e) {
-                e.printStackTrace();
-            }
-            System.out.println("Finished");
-        }
-    }
-
-    public String test(){
-
-
-        return "This";
     }
 
 
-
-    public void init(KeyStore ks) {
+    public void init(KeyStore ks,char[] password) {
         /*Specification: initializes the library before its first use.
         This method should receive a reference to a key store that must contain the private and public key
         of the user, as well as any other parameters needed to access this key store (e.g., its password)
@@ -67,19 +33,47 @@ public class PwmLib {
         These keys maintained by the key store will be the ones used in the following session of commands
         issued at the client side, until a close() function is called.
         */
-
+        this.ks = ks;
+        this.ksPassword = password;
+        Registry registry = LocateRegistry.getRegistry("127.0.0.1", 1099);
+        server = (ServerInterface) registry.lookup("PWMServer");
     }
 
-    public void register_user(){
+    public UUID register_user(){
         /*Specification: registers the user on the server, initializing the required data structures to
         securely store the passwords.*/
-
+        try {
+            this.userID = server.register(ks.getKey(CLIENT_PUBLIC_KEY,ksPassword));
+            return userID;
+        } catch (RemoteException e) {
+            e.printStackTrace();
+        } catch (KeyStoreException e) {
+            e.printStackTrace();
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        } catch (UnrecoverableKeyException e) {
+            e.printStackTrace();
+        }
     }
 
     public void save_password (byte[] domain, byte[] username, byte[] password){
         /*Specification: stores the triple (domain, username, password) on the server. This corresponds
         to an insertion if the (domain, username) pair is not already known by the server, or to an update otherwise.
         */
+
+        try {
+            server.put(ks.getKey(CLIENT_PUBLIC_KEY,ksPassword),domain,username,password);
+        } catch (RemoteException e) {
+            e.printStackTrace();
+        } catch (KeyStoreException e) {
+            e.printStackTrace();
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        } catch (UnrecoverableKeyException e) {
+            e.printStackTrace();
+        }
+
+
     }
 
 
@@ -88,9 +82,17 @@ public class PwmLib {
         what should happen if the (domain, username) pair does not exist is unspecified
         */
 
-        byte [] example = new byte[10];
-
-        return example;
+        try {
+            return server.get(ks.getKey(CLIENT_PUBLIC_KEY,this.ksPassword),domain,username);
+        } catch (RemoteException e) {
+            e.printStackTrace();
+        } catch (KeyStoreException e) {
+            e.printStackTrace();
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        } catch (UnrecoverableKeyException e) {
+            e.printStackTrace();
+        }
     }
 
     public void close(){
